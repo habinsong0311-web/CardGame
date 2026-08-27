365ea4f85305c7a0b8e6790b7c1f9e292fdd8efe
habinsong0311-web <habinsong0311@gmail.com>
2026-08-27 11:12:02 +0900

    [Add]Deck
    기본 덱 추가

--- Assets/Script/Deck.cs
+++ Assets/Script/Deck.cs
@@ -1,13 +1,33 @@
 using UnityEngine;
-
+using System.Collections.Generic;
+[System.Serializable] public class DeckEntry
+{
+    [SerializeField] private CardSetting card; //들어갈 카드
+    [SerializeField, Range(1, 3)] private int count; // 카드 개수는 1~3
+    public CardSetting Card => card;
+    public int Count => count;
+}
 public class Deck : MonoBehaviour
 {
-    void Start()
+    [Header("덱 구성")]
+    [SerializeField] private List<DeckEntry> deckEntries = new List<DeckEntry>();
+    private List<CardSetting> remainingCards = new List<CardSetting>();
+
+    private void CreateDeck()
     {
-        
-    }
-    void Update()
-    {
-        
+        remainingCards.Clear();
+        foreach (DeckEntry entry in deckEntries)
+        {
+            if (entry.Card == null)
+            {
+                continue;
+            }
+            for (int i = 0; i < entry.Count; i++)
+            {
+                remainingCards.Add(entry.Card);
+            }
+
+        }
     }
 }
+
